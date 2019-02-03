280d7ac4bfe9231b8a3c1c0d7c243e642ce7d643
Edward J <edjeong99@gmail.com>
2019-02-03 14:00:27 -0500

    skip authenticate for debugging

--- server.js
+++ server.js
@@ -7,17 +7,16 @@ const server = require('express')();
 const errorHandler = require('./helpers/errorHandler');
 const notesRouter = require('./notes/notesRouter.js');
 const authRouter = require('./auth/authRouter.js');
-const {authenticate} = require('./auth/middlewares');
-
+const { authenticate } = require('./auth/middlewares');
 
 //* Middleware & Routes
 require('./middleware')(server);
 
 server.use('/auth', authRouter);
-server.use('/api', authenticate, notesRouter);
-
+// server.use('/api', authenticate, notesRouter);
+server.use('/api', notesRouter);
 
 //* Error Handler
-server.use(errorHandler)
+server.use(errorHandler);
 
-module.exports = server
+module.exports = server;
